585a7e7ec8696c1c75ccdb87cf50e5a814f8f7f1
ASHIQNC <ashiqrahman55@gmail.com>
2025-06-19 09:40:57 +0530

    Changed the color of heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -126,7 +126,7 @@ export default function Home() {
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="flex items-baseline justify-between border-b  border-gray-200pb-6 pt-24">
         {/* tracking-tight:letter spacing */}
-        <h1 className="text-xl sm:text-4xl font-bold tracking-tight text-gray-900">
+        <h1 className="text-xl sm:text-4xl font-bold tracking-tight text-red-900">
           High-quality cotton selection
         </h1>
 
@@ -177,7 +177,7 @@ export default function Home() {
 
       <section className="pb-24 pt-6">
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
-          {/* filters...... */}
+          {/* filters */}
           <div className="hidden lg:block">
             <ul className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
               {SUBCATEGORIES.map((category) => (
